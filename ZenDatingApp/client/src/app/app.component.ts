0cc9f2abfd63be8aa01e19e50b95693cbf785258
achooStorm <114879202+achooStorm@users.noreply.github.com>
2022-12-11 13:28:53 -0600

    Fixing some errors

--- ZenDatingApp/client/src/app/app.component.ts
+++ ZenDatingApp/client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { response } from 'express';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Zen Dating App';
+export class AppComponent implements OnInit {
+
+  title: string = 'Zen Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {} 
+
+  
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next: () => this.users = response,
+      error: () => console.log(Error),
+      complete: () => console.log('Request has completed')
+    })
+  }
+
+  
 }
