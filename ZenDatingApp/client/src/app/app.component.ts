b4f900d5b0a08a259f2aa40e3122886f1f372674
achooStorm <114879202+achooStorm@users.noreply.github.com>
2022-12-12 13:15:49 -0600

    Created the client login and register and working on interface

--- ZenDatingApp/client/src/app/app.component.ts
+++ ZenDatingApp/client/src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { response } from 'express';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -8,20 +8,20 @@ import { response } from 'express';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  title = 'Dating app';
 
-  title: string = 'Zen Dating App';
-  users: any;
+  constructor(private accountService: AccountService) {}
 
-  constructor(private http: HttpClient) {} 
-
-  
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: () => this.users = response,
-      error: () => console.log(Error),
-      complete: () => console.log('Request has completed')
-    })
+    this.setCurrentUser();
   }
 
-  
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
+  }
+
+
 }
